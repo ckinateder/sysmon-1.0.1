3919155e4856907f261c5efd8644f43cf35083d9
Calvin Kinateder <calvinkinateder1@yahoo.com>
2015-06-11 09:52:48 -0400

    Pip/module installation bugfixes

--- Setup.py
+++ Setup.py
@@ -20,8 +20,8 @@ if os.path.exists ('./sysmon.py') == False:
 if os.path.exists('/sys/class/thermal/thermal_zone0/temp') == False:
 	print "Sorry, your hardware is not yet supported.\nProceed with alternate installation? [Y/n]"
 	i = input()
-	g = "'"+str(i)+"'"
-	if str(g) == 'Y' or str(g) == 'y':
+	#g = "'"+str(i)+"'"
+	if str(i) == Y or str(i) == 'y':
 		print "Proceeding..."
 	else:
 		sys.exit()
